3c15a15918841ad6a6f3024b121001d132cc9231
crgalloway <crgalloway@gmail.com>
2018-01-29 13:37:23 -0800

    discovered significant math errors in fewBillion.js

--- fewBillion.js
+++ fewBillion.js
@@ -1,14 +1,21 @@
-var sum=0.01;
-for(var i=1; i<=30; i++){
-	sum*=2;
+var reward=0.01;
+var sum=0;
+for(var i=0; i<=30; i++){
+	sum+=reward;
+	reward*=2;
 	if(sum >=10000 && sum<11000){
 		var days = i;
 	}
 }
-console.log(sum);
-console.log("It takes "+i+" days to reach $10,000")
+console.log("$"+sum+" at 30 days.");
+console.log("It takes "+days+" days to reach $10,000")
+
 for(var i=0; i<=10000; i++){
-	sum*=2;	
+	reward*=2;	
+	sum+=reward;
+	if(sum >=10000000000 && sum<11000000000){
+		console.log("It takes "+(i+30)+" days to reach $10,000,000,000")
+	}
 	if(sum == Infinity){
 		console.log("It takes "+(i+30)+" days to reach infinity");
 		break;
